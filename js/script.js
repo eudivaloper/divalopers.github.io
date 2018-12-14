7a2f53bb81ae92050e71d7813dd53ed25318b34a
Jessica Lopes <jeesousa91@gmail.com>
2018-12-14 17:17:28 -0200

    up login

--- js/script.js
+++ js/script.js
@@ -48,3 +48,14 @@
   })
 
 })(jQuery); // End of use strict
+
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
