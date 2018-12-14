df22b17bc4f972e9c857806a833a0e04ef13ac80
Jessica Lopes <jeesousa91@gmail.com>
2018-12-14 17:44:21 -0200

    sign up

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@
 
 
 // Get the modal
-var modal = document.getElementById('id01');
+var modal = document.getElementById('id01, id02');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
